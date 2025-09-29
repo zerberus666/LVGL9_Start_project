5bcce777e040041c69f8d0880711d7cf93aaf67a
zerberus <frzh92@gmail.com>
2025-09-29 14:06:45 +0300

    initial

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ lv_obj_t * label;
 
 static void btn_event_cb(lv_event_t * e) {
     lv_obj_t * btn = lv_event_get_target(e);
-    lv_obj_t * lbl = lv_obj_get_child(btn, 0); // We assume that Label is the first daughter element
+    lv_obj_t * lbl = lv_obj_get_child(btn, 0); // предполагаем, что label Ч первый дочерний элемент
     const char * text = lv_label_get_text(lbl);
     printf("Button pressed: %s\n", text);
     fflush(stdout);
 
 
-    lv_label_set_text(label, text);
+    lv_label_set_text(label, text); // мен€ем надпись сверху
 }
 
 
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
     printf("Test-------->\n");
 
     label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello LVGL 2!");
+    lv_label_set_text(label, "Hello LVGL!");
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 10);
 
     /* Buttons */
